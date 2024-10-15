3f2b4f38fa4ad363165c4d365a1c10a4aee6717d
Siddardha Nelapudi <156110982+Siddardha5@users.noreply.github.com>
2024-10-15 15:49:27 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My dearest Chintalli,
-Every day with you feels like a beautiful dream come true. Your smile brightens my world, and your love fills my heart with indescribable joy. I cherish every moment we spend together and find myself falling deeper in love with you as time goes by.
-Chintalli, you are my sunshine, my rock, and my best friend. I love you more than words can express, and I'm grateful for your presence in my life. You make everything better just by being you.
-Forever yours, Sid")
+st.title("🎈 My dearest Chintalli, Every day with you feels like a beautiful dream come true. Your smile brightens my world, and your love fills my heart with indescribable joy. I cherish every moment we spend together and find myself falling deeper in love with you as time goes by. Chintalli, you are my sunshine, my rock, and my best friend. I love you more than words can express, and I'm grateful for your presence in my life. You make everything better just by being you. Forever yours, Sid")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
